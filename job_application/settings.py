c7331746d684f5ff554e7e515f92a1113d117753
mustapha jay <mustaphajay2@gmail.com>
2022-11-26 16:25:35 +0100

    whitenoise added

--- job_application/settings.py
+++ job_application/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,16 +120,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-# STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# STATICFILES_DIRS = [
-#    os.path.join(BASE_DIR, 'static')
-# ]
-
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT =  BASE_DIR/'staticfiles'
 STATICFILES_DIRS = [
-    BASE_DIR / 'staticfiles'
+   os.path.join(BASE_DIR, 'static')
 ]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+# STATIC_URL = 'static/'
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static'
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
